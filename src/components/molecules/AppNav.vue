1ad551b0555659a06fc2d59ad43c66224ca57efb
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-11 17:13:35 -0300

    feat: ensure Header displays company name

---
+++ src/components/molecules/AppNav.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data: () => ({
+    companyName: "Vue Careers"
+  })
+}
+</script>
+
+<template>
+  <header role="banner">
+    <div class="h-14 border-b border-zinc-300 px-4">
+      <a
+        href="#"
+        class="flex h-full w-fit items-center gap-2"
+        tabindex="0"
+      >
+        <div class="font-sans text-lg tracking-tight text-zinc-600 lg:text-xl">
+          {{ companyName }}
+        </div>
+      </a>
+    </div>
+  </header>
+</template>
