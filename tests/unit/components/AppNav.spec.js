d0e1ae566246c6d369d156dd90b3af43720f3f80
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-11 18:25:32 -0300

    test: ensure Header displays menu items for navigation

--- tests/unit/components/AppNav.spec.js
+++ tests/unit/components/AppNav.spec.js
@@ -10,4 +10,20 @@ describe("AppNav", () => {
 
     expect(wrapper.text()).toMatch("Vue Careers")
   })
+
+  it("should display menu items for navigation", () => {
+    const wrapper = mount(AppNav)
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    )
+    const navigationMenuText = navigationMenuItems.map((item) => item.text())
+
+    expect(navigationMenuText).toEqual([
+      "Teams",
+      "Locations",
+      "Benefits",
+      "Jobs",
+      "Students"
+    ])
+  })
 })
