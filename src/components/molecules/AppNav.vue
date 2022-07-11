a3576f5a73d13abffcbe541c2395dbf8c9d4a38a
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-11 18:25:50 -0300

    feat: ensure Header displays menu items for navigation

--- src/components/molecules/AppNav.vue
+++ src/components/molecules/AppNav.vue
@@ -1,14 +1,15 @@
 <script>
 export default {
   data: () => ({
-    companyName: "Vue Careers"
+    companyName: "Vue Careers",
+    menuItems: ["Teams", "Locations", "Benefits", "Jobs", "Students"]
   })
 }
 </script>
 
 <template>
   <header role="banner">
-    <div class="h-14 border-b border-zinc-300 px-4">
+    <div class="flex h-14 border-b border-zinc-300 px-4 lg:h-16">
       <a
         href="#"
         class="flex h-full w-fit items-center gap-2"
@@ -18,6 +19,26 @@ export default {
           {{ companyName }}
         </div>
       </a>
+      <nav
+        arial-label="Main navigation"
+        class="ml-12 h-full flex-1"
+      >
+        <ul class="flex h-full items-center gap-9 text-center font-sans">
+          <li
+            v-for="menuItem in menuItems"
+            :key="menuItem"
+            class="block h-full"
+            data-test="main-nav-list-item"
+          >
+            <a
+              href="#"
+              class="flex h-full items-center py-2.5 text-[0.875rem] text-zinc-500 transition-colors duration-200 hover:text-zinc-900"
+            >
+              {{ menuItem }}
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
