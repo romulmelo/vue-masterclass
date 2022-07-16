bce3c3583131e86b3747b1c79d902b762538f341
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-16 18:43:41 -0300

    test: ensure Nav has a Logo component

--- tests/unit/components/AppNav.spec.js
+++ tests/unit/components/AppNav.spec.js
@@ -3,8 +3,15 @@
 import { mount } from "@vue/test-utils"
 
 import AppNav from "@/components/molecules/AppNav.vue"
+import AppLogo from "@/components/atoms/AppLogo.vue"
 
 describe("AppNav", () => {
+  it("should have a logo", () => {
+    const wrapper = mount(AppNav)
+
+    expect(wrapper.findComponent(AppLogo).exists()).toBe(true)
+  })
+
   it("should display menu items for navigation", () => {
     const wrapper = mount(AppNav)
     const navigationMenuItems = wrapper.findAll(
