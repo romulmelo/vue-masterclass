2d96b185f37e93771aa32389ac1c4c738abeedcd
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-08-04 18:31:56 -0300

    feat: ensure SubNav displays items for navigation

---
+++ src/components/molecules/AppSubnav.vue
@@ -0,0 +1,47 @@
+<script>
+import { Icon } from "@iconify/vue"
+export default {
+  components: { Icon },
+  data: () => ({
+    list: [
+      { label: "Saved Jobs", icon: "bookmark" },
+      { label: "Jobs Alert", icon: "notifications-rounded" }
+    ]
+  })
+}
+</script>
+
+<template>
+  <div class="molecule-subnav">
+    <div class="molecule-subnav__container">
+      <div></div>
+      <nav class="molecule-subnav__nav">
+        <ul class="molecule-subnav__list">
+          <li
+            v-for="listItem in list"
+            :key="listItem.label"
+            class="molecule-subnav__item"
+            data-test="subnav-list-item"
+          >
+            <a
+              class="molecule-subnav__link"
+              href="#"
+              :aria-label="listItem.label"
+            >
+              <Icon
+                :icon="`material-symbols:${listItem.icon}`"
+                width="24"
+                height="24"
+              />
+              <span>{{ listItem.label }}</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/components/molecules/molecule-subnav.scss";
+</style>
