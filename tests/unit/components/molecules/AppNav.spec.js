17856afa1a575a57818d2cc9a9567366261c05df
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-08-04 09:34:25 -0300

    refactor: add custom class to Nav component

--- tests/unit/components/molecules/AppNav.spec.js
+++ tests/unit/components/molecules/AppNav.spec.js
@@ -1,12 +1,10 @@
-// @vitest-environment happy-dom
-
 import { mount } from "@vue/test-utils"
 
-import AppNavList from "@/components/molecules/AppNavList.vue"
+import AppNav from "@/components/molecules/AppNav.vue"
 
-describe("AppNavList", () => {
+describe("AppNav", () => {
   it("should display menu items for navigation", () => {
-    const wrapper = mount(AppNavList)
+    const wrapper = mount(AppNav)
     const navigationMenuItems = wrapper.findAll("[data-test='nav-list-item']")
     const navigationMenuText = navigationMenuItems.map((item) => item.text())
 
@@ -17,15 +15,5 @@ describe("AppNavList", () => {
       "Jobs",
       "Students"
     ])
-  })
-
-  it("should change display to vertical", () => {
-    const wrapper = mount(AppNavList, {
-      props: {
-        isVertical: true
-      }
-    })
-
-    expect(wrapper.classes()).toContain("vc-nav--vertical")
   })
 })
