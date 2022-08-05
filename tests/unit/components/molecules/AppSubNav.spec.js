3ae33caa83dd6d13d5e3a2617b4b26cc562fb7cd
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-08-04 21:04:19 -0300

    test: ensure SubNav displays a prompt to alert and save jobs when user is logged

--- tests/unit/components/molecules/AppSubNav.spec.js
+++ tests/unit/components/molecules/AppSubNav.spec.js
@@ -3,17 +3,10 @@ import { mount } from "@vue/test-utils"
 import AppSubNav from "@/components/molecules/AppSubnav.vue"
 
 describe("AppSubNav", () => {
-  it("should display items for navigation", () => {
-    const wrapper = mount(AppSubNav)
-    const navigationMenuItem = wrapper.findAll('[data-test="subnav-list-item"]')
-    const navigationMenuLink = navigationMenuItem.map((item) => item.find("a"))
-    const navigationMenuText = navigationMenuItem.map((item) => item.text())
+  let wrapper
 
-    expect(navigationMenuItem.length).toBe(2)
-    expect(navigationMenuText).toEqual(["Saved Jobs", "Jobs Alert"])
-    expect(
-      navigationMenuLink.map((link) => link.attributes("aria-label"))
-    ).toEqual(["Saved Jobs", "Jobs Alert"])
+  beforeEach(() => {
+    wrapper = mount(AppSubNav)
   })
 
   describe("when user is on job page", () => {
@@ -29,10 +22,32 @@ describe("AppSubNav", () => {
 
   describe("when user is not on job page", () => {
     it("should display a link to job results page", () => {
-      const wrapper = mount(AppSubNav)
       const jobMessage = wrapper.find('[data-test="subnav-job-message"]')
 
       expect(jobMessage.text()).toMatch("Find a job at Google")
+    })
+  })
+
+  describe("when user is logged in", () => {
+    it("should display prompt to alert and save jobs", () => {
+      const wrapper = mount(AppSubNav, {
+        props: {
+          logged: true
+        }
+      })
+      const navigationMenuItem = wrapper.findAll(
+        '[data-test="subnav-list-item"]'
+      )
+      const navigationMenuLink = navigationMenuItem.map((item) =>
+        item.find("a")
+      )
+      const navigationMenuText = navigationMenuItem.map((item) => item.text())
+
+      expect(navigationMenuItem.length).toBe(2)
+      expect(navigationMenuText).toEqual(["Saved Jobs", "Jobs Alert"])
+      expect(
+        navigationMenuLink.map((link) => link.attributes("aria-label"))
+      ).toEqual(["Saved Jobs", "Jobs Alert"])
     })
   })
 })
