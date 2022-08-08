ee5183e5eaeb40d66bf88f5ccf13c407b0829600
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-08-08 12:40:25 -0300

    feat: ensure Drawer displays a logo

---
+++ src/components/molecules/AppDrawer.vue
@@ -0,0 +1,19 @@
+<script>
+import AppLogo from "@/components/atoms/AppLogo.vue"
+export default { components: { AppLogo } }
+</script>
+
+<template>
+  <div class="molecule-drawer">
+    <div class="molecule-drawer__container">
+      <div class="molecule-drawer__logo">
+        <AppLogo />
+      </div>
+    </div>
+  </div>
+  <div class="molecule-drawer-backdrop"></div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/components/molecules/molecule-drawer.scss";
+</style>
