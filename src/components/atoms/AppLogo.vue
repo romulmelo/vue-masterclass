924976991da938e39ee3b8c35a66a19366a50585
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-08-11 10:31:33 -0300

    refactor: change from css scoped to css modules

--- src/components/atoms/AppLogo.vue
+++ src/components/atoms/AppLogo.vue
@@ -13,17 +13,23 @@ export default {
   <a
     :title="companyName"
     :aria-label="companyName"
-    class="atom-logo"
+    :class="$style.logo"
     href="https://careers.google.com/"
     tabindex="0"
   >
     <Icon icon="logos:vue" width="24" height="24" aria-label="Vue Logo" />
-    <div class="atom-logo__title">
+    <div :class="$style.logoText">
       {{ companyName }}
     </div>
   </a>
 </template>
 
-<style lang="scss" scoped>
-@import "@/assets/styles/components/atoms/atom-logo.scss";
+<style lang="css" module>
+.logo {
+  @apply flex h-full w-fit items-center gap-2;
+}
+
+.logoText {
+  @apply whitespace-nowrap font-sans text-lg tracking-tight text-zinc-600 lg:text-xl;
+}
 </style>
--- src/components/molecules/AppDrawer.vue
+++ src/components/molecules/AppDrawer.vue
@@ -4,16 +4,30 @@ export default { components: { AppLogo } }
 </script>
 
 <template>
-  <div class="molecule-drawer">
-    <div class="molecule-drawer__container">
-      <div class="molecule-drawer__logo">
+  <div :class="$style.drawer">
+    <div :class="$style.drawerContainer">
+      <div :class="$style.drawerLogo">
         <AppLogo />
       </div>
     </div>
   </div>
-  <div class="molecule-drawer-backdrop"></div>
+  <div :class="$style.drawerBackdrop"></div>
 </template>
 
-<style lang="scss" scoped>
-@import "@/assets/styles/components/molecules/molecule-drawer.scss";
+<style lang="css" module>
+.drawer {
+  @apply fixed top-0 left-0 right-14 bottom-0 z-20 max-w-[280px] bg-white;
+}
+
+.drawerContainer {
+  @apply h-full;
+}
+
+.drawerLogo {
+  @apply h-14 border-b border-zinc-300 px-4 lg:h-16;
+}
+
+.drawerBackdrop {
+  @apply fixed inset-0 z-10 bg-black/40;
+}
 </style>
