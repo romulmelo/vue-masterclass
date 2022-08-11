4a2be7e95b5d8de8fee104c0f23c82753801c840
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-08-11 10:47:15 -0300

    test: ensure Drawer displays a list of links

--- tests/unit/components/molecules/AppDrawer.spec.js
+++ tests/unit/components/molecules/AppDrawer.spec.js
@@ -12,4 +12,18 @@ describe("AppDrawer", () => {
   it("should display a logo", () => {
     expect(wrapper.findComponent({ name: "AppLogo" }).exists()).toBe(true)
   })
+
+  it("should display a list of links", () => {
+    const links = wrapper.findAll('[data-test="drawer-link"]')
+    const linkText = links.map((link) => link.text())
+
+    expect(links.length).toBe(5)
+    expect(linkText).toEqual([
+      "Teams",
+      "Locations",
+      "Benefits",
+      "Jobs",
+      "Students"
+    ])
+  })
 })
